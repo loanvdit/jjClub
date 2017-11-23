4b3064a4d2dc47d3173c3849c9aed5bba217c3b0
Vu Dinh Loan <loanvdit@gmail.com>
2017-11-24 03:38:34 +0700

    top page

---
+++ js/top.js
@@ -0,0 +1,41 @@
+(function($) {
+    $(function() {
+        /*
+        Carousel initialization
+        */
+        $('.jcarousel')
+            .jcarousel({
+                // Options go here
+            });
+
+        /*
+         Prev control initialization
+         */
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '-=1'
+            });
+
+        /*
+         Next control initialization
+         */
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                // Options go here
+                target: '+=1'
+            });
+    });
+})(jQuery);
