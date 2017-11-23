8d17ffe6e1c8ab721682e7e714daeb478a47feff
Vu Dinh Loan <loanvdit@gmail.com>
2017-11-24 00:45:44 +0700

    Fix responsive Index

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 $( document ).ready(function() {
     $('.panel-group>.panel .panel-title>a').click(function(){
-        console.log($(this))
         $($(this).children('.fa')).toggleClass('fa-angle-down')
-        .toggleClass('fa-angle-up')
-    })
+        .toggleClass('fa-angle-up');
+
+        $('.rd_first').each(function() {
+          $(this).trigger("click");
+        });
+
+    });
+
     $('#myModal').modal();
     $('.modal-backdrop').appendTo('.body');   
     //remove the padding right and modal-open class from the body tag which bootstrap adds when a modal is shown
     $('body').removeClass("modal-open")
     $('body').css("padding-right","");  
+
+    // Click event under 18
+    $('.btn-u').click(function() {
+        window.location.href = "error.html";
+    });
 });
